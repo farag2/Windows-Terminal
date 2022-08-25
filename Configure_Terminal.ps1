cb514e2ea293b01f9619c148458642b3ecf3f735
Dmitry Nefedov <nefedovds@ya.ru>
2022-08-25 22:42:53 +0300

    Update Configure_Terminal.ps1

--- Configure_Terminal.ps1
+++ Configure_Terminal.ps1
@@ -262,7 +262,7 @@ if ((New-Object -TypeName System.Drawing.Text.InstalledFontCollection).Families.
 	}
 	else
 	{
-		$Terminal.profiles.defaults.font | Add-Member -Name face-MemberType NoteProperty -Value "FiraCode Nerd Font Mono Retina" -Force
+		$Terminal.profiles.defaults.font | Add-Member -Name face -MemberType NoteProperty -Value "FiraCode Nerd Font Mono Retina" -Force
 	}
 }
 
